54cd94793ad4d24e994733845f8c65cb97e37896
Simon Masson <simon.masson@yahoo.fr>
2021-04-22 19:47:29 +0200

    simple signature works

--- src/bls.rs
+++ src/bls.rs
@@ -8,6 +8,19 @@ use bls12_381::{
     G2Projective, Gt, Scalar,
 };
 
+use rand::{Rng, thread_rng, SeedableRng, seq::IteratorRandom};
+
+// Fixed seed for reproducability
+fn rng() -> rand::rngs::StdRng {
+    rand::rngs::StdRng::seed_from_u64(0)
+}
+
+pub fn random_scalar<R: Rng>(rng: &mut R) -> Scalar {
+    let mut buf = [0; 64];
+    rng.fill_bytes(&mut buf);
+    Scalar::from_bytes_wide(&buf)
+}
+
 fn hash_to_scalar(msg: &[u8], dst: &[u8]) -> Scalar {
     hash_to_field::<Scalar, ExpandMsgXmd<sha2::Sha256>>(msg, dst, 1)[0]
 }
@@ -48,11 +61,6 @@ fn compressed_bytevec(g1: &G1Affine) -> Vec<u8> {
 // sign a message with signature in G2
 pub fn sign_g2(secret: Scalar, msg: &[u8]) -> G2Affine {
     (hash_to_g2(msg) * secret).into()
-}
-
-// sign a message with membership key and signature in G2
-pub fn sign_with_mk_g2(secret: Scalar, mk: G2Affine, msg: &[u8]) -> G2Affine {
-    (sign_g2(secret, msg) + G2Projective::from(mk)).into()
 }
 
 // verify a signature in G2 against a public key in G1
@@ -177,112 +185,6 @@ impl Setup {
         [&capk_bytes[..], &msg[..]].concat()
     }
 
-    // The message to be signed for the `i`th member's membership key fragments
-    fn memkey_frag_msg(&self, i: usize) -> Vec<u8> {
-        self.prefix_apk(&(i as u64).to_le_bytes())
-    }
-
-    // The messages to be signed for each member's membership key fragments
-    fn memkey_frag_msgs(&self) -> Vec<Vec<u8>> {
-        (0..self.members())
-            .map(|i| self.memkey_frag_msg(i))
-            .collect()
-    }
-
-    // Generate the membership key fragments for each member from a keypair
-    pub fn memkey_frags(&self, keys: &Keypair) -> Vec<G2Affine> {
-        let coeff = self
-            .coeff(&keys.pubkey)
-            .expect("Public key not found in setup");
-        // sign with secret * coeff
-        let sign = |msg: &Vec<u8>| sign_g2(keys.secret * coeff, &msg);
-        self.memkey_frag_msgs().iter().map(sign).collect()
-    }
-
-    // verify the `i`th membership key
-    fn verify_memkey(&self, i: usize, mk: &G2Affine) -> bool {
-        verify_g2(self.apk(), &mk, &self.memkey_frag_msg(i))
-    }
-
-    // Attempt to construct the `i`th membership key from its fragments
-    pub fn memkey(&self, i: usize, frags: &[G2Affine]) -> Option<G2Affine> {
-        let mk = frags.iter().sum_by(G2Projective::from).into();
-
-        if self.verify_memkey(i, &mk) {
-            Some(mk)
-        } else {
-            None
-        }
-    }
-
-    pub fn prepare_threshold_verification(
-        &self,
-        threshold: usize,
-        positions: &[usize],
-        msg: &[u8],
-        res_appk: &mut G1Affine,
-        res_msg_hash: &mut G2Affine,
-        res_apk: &mut G1Affine,
-        res_mf_hash_sum: &mut G2Affine,
-    ) -> bool {
-        // sort and deduplicate positions
-        let mut positions = positions.to_vec();
-        positions.sort();
-        positions.dedup();
-        if positions.len() < threshold {
-            false
-        } else {
-            *res_apk = self.apk;
-            let pubkeys = &self.pubkeys;
-            // compute the aggregated participant pubkey
-            let ppks = positions.iter().map(|i| pubkeys[*i]);
-            *res_appk = ppks.sum_by(G1Projective::from).into();
-            // the hash of the message prefixed by the compressed apk
-            *res_msg_hash = hash_to_g2(&self.prefix_apk(msg));
-
-            // the sum of the hashes of memkey fragment messages
-            *res_mf_hash_sum = positions
-                .into_iter()
-                .sum_by(|i: usize| -> G2Projective {
-                    hash_to_g2(&self.memkey_frag_msg(i)).into()
-                })
-                .into();
-            true
-        }
-    }
-
-    /* Verify a threshold signature constructed from participants with the
-     * given positions in the setup
-     * See the AMS of section 4.3 of the eprint 2018/483.
-     */
-    pub fn verify_threshold(
-        &self,
-        threshold: usize,
-        sig: &G2Affine,
-        positions: &[usize],
-        msg: &[u8],
-    ) -> bool {
-        let mut appk = G1Affine::identity();
-        let mut msg_hash = G2Affine::identity();
-        let mut apk = G1Affine::identity();
-        let mut mf_hash_sum = G2Affine::identity();
-        let foo: bool = self.prepare_threshold_verification(
-            threshold,
-            positions,
-            msg,
-            &mut appk,
-            &mut msg_hash,
-            &mut apk,
-            &mut mf_hash_sum,
-        );
-        let ml = multi_miller_loop(&[
-            (&-G1Affine::generator(), &(*sig).into()),
-            (&appk, &msg_hash.into()),
-            (&apk, &mf_hash_sum.into()),
-        ]);
-        foo && Gt::identity() == ml.final_exponentiation()
-    }
-
     /*
      * verify a signature `sig` from the participants of the group
      * defined in the setup.
@@ -295,75 +197,6 @@ impl Setup {
         }
         foo && verify_g2(&self.apk, sig, &self.prefix_apk(msg))
     }
-}
-
-/*
- * Verify multiple aggregated signatures from different setups in 2*n+1
- * pairing computations instead of 3*n
- */
-pub fn verify_multi_threshold_sig(
-    setups: &[&Setup],
-    thresholds: &[usize],
-    sigs: &[G2Affine],
-    positions: &[&[usize]],
-    msgs: &[&[u8]],
-) -> bool {
-    let n = (*setups).len();
-    assert!(n > 0);
-    assert_eq!(n, thresholds.len());
-    assert_eq!(n, sigs.len());
-    assert_eq!(n, positions.len());
-    assert_eq!(n, msgs.len());
-
-    let sig: G2Affine = sigs.into_iter().sum_by(G2Projective::from).into();
-
-    let mut ml_g1 = vec![G1Affine::identity(); 2 * n + 1];
-    let mut ml_g2 = vec![G2Affine::identity().into(); 2 * n + 1];
-
-    ml_g1[0] = -G1Affine::generator();
-    ml_g2[0] = sig.into();
-
-    let mut foo1: bool = true;
-
-    for i in 0..n {
-        // sort and deduplicate positions
-        let mut positions = positions[i].to_vec();
-        positions.sort();
-        positions.dedup();
-        if positions.len() < thresholds[i] {
-            return false;
-        } else {
-            let mut appk = G1Affine::identity();
-            let mut msg_hash = G2Affine::identity();
-            let mut apk = G1Affine::identity();
-            let mut mf_hash_sum = G2Affine::identity();
-            foo1 = foo1
-                && (*setups)[i].prepare_threshold_verification(
-                    positions.len(),
-                    &positions,
-                    msgs[i],
-                    &mut appk,
-                    &mut msg_hash,
-                    &mut apk,
-                    &mut mf_hash_sum,
-                );
-            ml_g1[2 * i + 1] = appk;
-            ml_g2[2 * i + 1] = msg_hash.into();
-            ml_g1[2 * i + 2] = apk;
-            ml_g2[2 * i + 2] = mf_hash_sum.into();
-        }
-    }
-
-    let foo2: bool = Gt::identity()
-        == multi_miller_loop(
-            &ml_g1
-                .iter()
-                .zip(ml_g2.iter())
-                .collect::<Vec<(&G1Affine, &G2Prepared)>>(),
-        )
-        .final_exponentiation();
-
-    foo1 && foo2
 }
 
 pub fn verify_multi_aggregated_sig(
@@ -413,19 +246,68 @@ pub fn dealer_gen_keys(n: usize) {
     // associated for verifying a (complete) sig
 }
 
-pub fn partial_sign() {
-    // sign a message with a secret share s_i
+pub fn sign(
+    partial_sigs: &[G2Affine],
+    positions: &[usize]
+) -> G2Affine {
+    // Compute the lagrange coefficients in O(t²) using a naive algorithm
+    let mut tmp = Scalar::one();
+    let mut inv = Scalar::one();
+    let lagrange_0 : Vec<Scalar> = positions.iter().map(|j| {
+	tmp = Scalar::one();
+	for k in positions.iter() {
+	    if *k != *j {
+		tmp = tmp * Scalar::from(*k as u32);
+		inv = Scalar::invert(
+		    &(Scalar::from(*k as u32) - Scalar::from(*j as u32))
+		).unwrap();
+		tmp = tmp * inv;
+	    }
+	}
+	tmp
+    }).collect();
+    
+    // Compute the signature from the partial signatures `partial_sigs`
+    let mut sig = G2Projective::identity();
+    for (j, sigma) in (*partial_sigs).iter().enumerate() {
+    	sig = sig + (sigma * lagrange_0[j]);
+    }
+    sig.into()
 }
 
-pub fn sign() {
-    // produce a signature from partial sigs
+pub fn eval(poly: &[Scalar], x: Scalar) -> Scalar {
+    let mut res = Scalar::zero();
+    for coeff in poly.iter().rev() {
+	res = res * x + coeff;
+    }
+    res
 }
 
-pub fn verify() {
-    // verify a (partial or not) sig, given a public key
-}
 
 #[test]
-pub fn test_aaa() {
-    assert_eq!(1+1,2);
+pub fn test1() {
+    let n = 5;
+    let t = 3;
+    let mut rng = rng();
+    let mut rng_scalar = thread_rng();
+    let poly : Vec<Scalar> = (0..t)
+	.map(|i| random_scalar(&mut rng_scalar)).collect();
+    let secret = eval(&poly, Scalar::zero());
+    let secret_shares : Vec<Scalar> = (0..n)
+	.map(|i| eval(&poly, Scalar::from(i as u32))).collect();
+    
+    let msg = b"lorem ipsum";
+    let mut positions = (0..n).choose_multiple(&mut rng, t);
+    positions.sort();
+    
+    let mut sk = Scalar::zero();
+    let mut sigs: Vec<G2Affine> = vec![G2Affine::identity(); t];
+    for (cpt,i) in positions.iter().enumerate() {
+	sk = secret_shares[*i];
+	sigs[cpt] = sign_g2(sk, msg);
+    }
+
+    let sig = sign(&sigs, &positions);
+    assert!(verify_g2(&(G1Affine::generator() * secret).into(), &sig,
+		      msg));
 }
