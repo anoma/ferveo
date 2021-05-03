b711253af9702d0bea7d7e84102f01953f180d19
joe bebel <joseph.bebel@gmail.com>
2021-05-03 02:36:47 -0700

    fix benchmark

--- benches/kzg.rs
+++ benches/kzg.rs
@@ -29,42 +29,21 @@ pub fn bench_kzg(c: &mut Criterion) {
         let (pp, powers_of_h) = setup(degree, rng).unwrap();
         group.bench_function("setup", |b| b.iter(|| setup(degree, rng)));
 
-        let (ck, vk) = trim(&pp, degree);
+        //let (ck, vk) = KZG::trim(&pp, degree);
         //12 us
         //group.bench_function("trim", |b| b.iter(|| trim(&pp, degree)));
 
         for _ in 0..1 {
             let p = DensePolynomial::<Fr>::rand(degree, rng);
 
-            let (comm, rand) = KZG::commit(&ck, &p, None, None).unwrap();
+            let comm = g1_commit(&pp.powers_of_g, &p).unwrap();
             group.bench_function("commit", |b| {
-                b.iter(|| KZG::commit(&ck, &p, None, None))
+                b.iter(|| g1_commit(&pp.powers_of_g, &p))
             });
 
             let domain =
                 ark_poly::Radix2EvaluationDomain::<Fr>::new(total_weight)
                     .unwrap();
-
-            //5.5us
-            /*group.bench_function("domain::new", |b| {
-                b.iter(|| {
-                    ark_poly::Radix2EvaluationDomain::<Fr>::new(total_weight)
-                })
-            });*/
-
-            let openings = open_amortized(&ck, &p, n).unwrap();
-            group.bench_function("open amortized", |b| {
-                b.iter(|| open_amortized(&ck, &p, n))
-            });
-
-            group.bench_function("open amortized unnormalized", |b| {
-                b.iter(|| open_amortized_unnormalized(&ck, &p, n))
-            });
-
-            let affine_openings = openings
-                .iter()
-                .map(|p| (*p).into())
-                .collect::<Vec<G1Affine>>();
 
             let mut point = Fr::one();
 
@@ -76,105 +55,69 @@ pub fn bench_kzg(c: &mut Criterion) {
                 t *= domain.group_gen;
             }
 
-            /*group.bench_function("share domain", |b| { //23 us
-                b.iter(|| {
-                    let mut share_domain =
-                        Vec::with_capacity(1 << domain.log_size_of_group);
-                    let mut t = Fr::one();
-                    for _ in 0..1 << domain.log_size_of_group {
-                        share_domain.push(t);
-                        t *= domain.group_gen;
-                    }
-                })
-            });*/
-
-            /*let A_I = compute_ai(&share_domain);
-            group.bench_function("compute_ai", |b| {
-                b.iter(|| compute_ai(&share_domain))
-            });*/
-            let A_I = subproduct_tree(&share_domain);
-
-            let A_I_prime = derivative(&A_I.M);
-
-            //51 us
-            //group.bench_function("derivative", |b| b.iter(|| derivative(&A_I)));
-
-            /*let c_i = compute_ci(&share_domain, &A_I_prime);
-            group.bench_function("compute_ci", |b| {
-                b.iter(|| compute_ci(&share_domain, &A_I_prime))
-            });*/
+            let share_domain = SubproductDomain::new(share_domain);
 
             let evals = share_domain
+                .u
                 .iter()
                 .map(|x| p.evaluate(x))
                 .collect::<Vec<Fr>>();
 
-            let (poly, proof) = fast_interpolate_and_batch(
-                &share_domain,
-                &evals,
-                &openings,
-                &A_I,
-                None,
-            );
+            let poly = share_domain.fast_interpolate(&evals);
+
             group.bench_function("interpolate", |b| {
                 b.iter(|| {
-                    fast_interpolate_and_batch(
-                        &share_domain,
-                        &evals,
-                        &openings,
-                        &A_I,
-                        None,
+                    let poly = share_domain.fast_interpolate(&evals);
+                })
+            });
+            let proof =
+                AmortizedOpeningProof::new(&pp.powers_of_g, &p, &domain)
+                    .unwrap();
+
+            let proof = proof.combine(
+                &(0..1 << domain.log_size_of_group),
+                &evals,
+                &share_domain,
+            );
+
+            group.bench_function("open amortized", |b| {
+                b.iter(|| {
+                    let proof = AmortizedOpeningProof::new(
+                        &pp.powers_of_g,
+                        &p,
+                        &domain,
                     )
+                    .unwrap();
+
+                    let proof = proof.combine(
+                        &(0..1 << domain.log_size_of_group),
+                        &evals,
+                        &share_domain,
+                    );
                 })
             });
 
-            /*            group.bench_function("batch_proofs", |b| {
-                b.iter(|| batch_proofs(&c_i, &openings))
-            });
-            let poly = lagrange_interpolate(
-                &share_domain,
-                &share_domain
-                    .iter()
-                    .map(|x| p.evaluate(x))
-                    .collect::<Vec<Fr>>(),
-                &A_I,
-                &A_I_prime,
-            );
-            group.bench_function("interpolate", |b| {
-                b.iter(|| {
-                    lagrange_interpolate(
-                        &share_domain,
-                        &share_domain
-                            .iter()
-                            .map(|x| p.evaluate(x))
-                            .collect::<Vec<Fr>>(),
-                        &A_I,
-                        &A_I_prime,
-                    )
-                })
-            });*/
+            let poly_commit = g1_commit(&pp.powers_of_g, &poly).unwrap();
 
             assert!(check_batched(
                 &powers_of_h,
-                &ck,
-                &vk,
                 &comm,
-                &A_I.M,
-                &poly,
-                &proof.into(),
+                &g2_commit(&powers_of_h, &share_domain.t.m).unwrap(),
+                &poly_commit,
+                &proof.into()
             )
             .unwrap());
+
             group.bench_function("check_batch", |b| {
                 b.iter(|| {
-                    check_batched(
+                    assert!(check_batched(
                         &powers_of_h,
-                        &ck,
-                        &vk,
                         &comm,
-                        &A_I.M,
-                        &poly,
-                        &proof.into(),
+                        &g2_commit(&powers_of_h, &share_domain.t.m).unwrap(),
+                        &poly_commit,
+                        &proof.into()
                     )
+                    .unwrap());
                 })
             });
         }
