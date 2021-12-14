8199e6b58119c2ab074e66809896ae457ccd7546
joe bebel <55120843+joebebel@users.noreply.github.com>
2021-12-13 19:20:52 -0800

    benches

--- subproductdomain/benches/benchmarks.rs
+++ subproductdomain/benches/benchmarks.rs
@@ -1,118 +1,18 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use subproductdomain::*;
 
-use super::*;
 use ark_ec::PairingEngine;
 use ark_ff::{One, Zero};
 use ark_poly::polynomial::univariate::DensePolynomial;
-use ark_poly::Polynomial;
 use ark_poly::UVPolynomial;
 use ark_std::UniformRand;
 
 type Fr = <ark_bls12_381::Bls12_381 as PairingEngine>::Fr;
-#[test]
-fn test_inverse() {
-    let rng = &mut ark_std::test_rng();
-
-    for l in [1, 2, 3, 5, 19, 25, 101].iter() {
-        for degree in 0..(l + 4) {
-            for _ in 0..10 {
-                let p = DensePolynomial::<Fr>::rand(degree, rng);
-                let p_inv = inverse_mod_xl::<Fr>(&p, *l).unwrap();
-                let mut t = &p * &p_inv; // p * p^-1
-                t.coeffs.resize(*l, Fr::zero()); // mod x^l
-                assert_eq!(t.coeffs[0], Fr::one()); // constant term == 1
-                for i in t.iter().skip(1) {
-                    assert_eq!(*i, Fr::zero()); // all other terms == 0
-                }
-            }
-        }
-    }
-}
-
-#[test]
-fn test_divide() {
-    let rng = &mut ark_std::test_rng();
-
-    let degree = 100;
-    for g_deg in 1..100 {
-        let f = DensePolynomial::<Fr>::rand(degree, rng);
-        let mut g = DensePolynomial::<Fr>::rand(g_deg, rng);
-        *g.last_mut().unwrap() = Fr::one(); //monic
-
-        let (q, r) = fast_divide_monic::<Fr>(&f, &g);
-
-        let t = &(&q * &g) + &r;
-
-        for (i, j) in t.coeffs.iter().zip(f.coeffs.iter()) {
-            assert_eq!(*i, *j);
-        }
-    }
-}
-
-#[test]
-fn test_interpolate() {
-    let rng = &mut ark_std::test_rng();
-    for d in 1..100 {
-        let mut points = vec![];
-        let mut evals = vec![];
-        for _ in 0..d {
-            points.push(Fr::rand(rng));
-            evals.push(Fr::rand(rng));
-        }
-
-        let s = SubproductDomain::<Fr>::new(points);
-        let p = s.interpolate(&evals);
-
-        for (x, y) in s.u.iter().zip(evals.iter()) {
-            assert_eq!(p.evaluate(x), *y)
-        }
-    }
-}
-
-#[test]
-fn test_linear_combine() {
-    let rng = &mut ark_std::test_rng();
-    for d in 1..100 {
-        let mut u = vec![];
-        let mut c = vec![];
-        for _ in 0..d {
-            u.push(Fr::rand(rng));
-            c.push(Fr::rand(rng));
-        }
-        let s = SubproductDomain::<Fr>::new(u);
-        let f = s.linear_combine(&c);
-
-        let r = Fr::rand(rng);
-        let m = s.t.m.evaluate(&r);
-        let mut total = Fr::zero();
-        for (u_i, c_i) in s.u.iter().zip(c.iter()) {
-            total += m * *c_i / (r - u_i);
-        }
-        assert_eq!(f.evaluate(&r), total);
-    }
-}
-
-#[test]
-fn test_inv_lagrange() {
-    for d in 1..100 {
-        let mut u = vec![];
-        for _ in 0..d {
-            u.push(Fr::rand(rng));
-        }
-        let s = SubproductDomain::<Fr>::new(u);
-        let f = s.inverse_lagrange_coefficients();
-
-        for (i, j) in s.u.iter().zip(f.iter()) {
-            assert_eq!(s.prime.evaluate(i), *j);
-        }
-    }
-}
 
 pub fn bench_subproductdomain(c: &mut Criterion) {
     let mut group = c.benchmark_group("Subproduct domain benchmarks");
     group.sample_size(10);
-    group.measurement_time(core::time::Duration::new(60, 0));
+    group.measurement_time(core::time::Duration::new(2, 0));
 
     let rng = &mut ark_std::test_rng();
 
@@ -123,10 +23,17 @@ pub fn bench_subproductdomain(c: &mut Criterion) {
             points.push(Fr::rand(rng));
             evals.push(Fr::rand(rng));
         }
-        let s = SubproductDomain::<Fr>::new(u);
-        group.bench_function(format!("New SubproductDomain d = {}", d), |b| {
-            b.iter(|| black_box(SubproductDomain::<Fr>::new(points)))
-        });
+        let s = SubproductDomain::<Fr>::new(points.clone());
+        group.bench_function(
+            format!("New SubproductDomain d = {}", d),
+            move |b| {
+                b.iter_batched(
+                    || points.clone(),
+                    |data| black_box(SubproductDomain::<Fr>::new(data)),
+                    criterion::BatchSize::SmallInput,
+                )
+            },
+        );
 
         group.bench_function(format!("interpolate d = {}", d), |b| {
             b.iter(|| black_box(s.interpolate(&evals)))
